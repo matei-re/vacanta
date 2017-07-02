4f2378475c4a8f0caa4db71a802ac4be687c530b
Raluca Matei <re.matei@gmail.com>
2017-07-02 16:13:46 +0300

    Am creat un unit test

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,8 @@ namespace Grades
             book.AddGrade(40);
             book.AddGrade(80.1f);
             book.AddGrade(76);
-
+            SpeechSynthesizer spune = new SpeechSynthesizer();
+            spune.Speak("Hello Mara! I love you!");
             GradeStatistic stats = book.ComputeStatistic();
             Console.WriteLine("media "+stats.AverageGrade);
             Console.WriteLine(stats.HighGrade);
---
+++ Grades/GradesTests/GradeBookTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests
+{
+    [TestClass()]
+    public class GradeBookTests
+    {
+        [TestMethod()]
+        public void ComputeStatisticTest()
+        {
+            //arrange
+            var a = new GradeBook();
+            a.AddGrade(5);
+            a.AddGrade(5);
+
+            //asume
+            var g = a.ComputeStatistic();
+            //assert
+
+
+             Assert.AreEqual(g.AverageGrade, 5);
+        }
+    }
+}
