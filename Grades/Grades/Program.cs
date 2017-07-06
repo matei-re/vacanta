a21cbe2c7f5545ba6cc24a2de678d7f16eb6b23b
Raluca Matei <re.matei@gmail.com>
2017-07-06 08:48:01 +0300

    Am transformat secvente de cod in metode. Dragut!!

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -13,25 +13,21 @@ namespace Grades
         static void Main(string[] args)
         {
             var book = new GradeBook();
-            try
-            {
-                Console.WriteLine("Ïntrodu un nume");
-                book.Name = Console.ReadLine();
-            }
-            catch(ArgumentException ex)
-                {
-                Console.WriteLine(ex.Message);
-
-            }
-            book.AddGrade(40);
-            book.AddGrade(80.1f);
-            book.AddGrade(76);
-            book.AddGrade(100);
+            GetBookName(book);
+            AddGrades(book);
             //   SpeechSynthesizer spune = new SpeechSynthesizer();
             //  spune.Speak("Hello Mara! I love you!");
-            StreamWriter outputFile = File.CreateText("Grades");
-            book.WriteGrades(outputFile);
-            outputFile.Close();
+            SaveGrades(book);
+            WriteGrades(book);
+
+            Console.ReadKey();
+
+
+
+        }
+
+        private static void WriteGrades(GradeBook book)
+        {
             GradeStatistic stats = book.ComputeStatistic();
             Console.WriteLine(book.Name);
             book.WriteGrades(Console.Out);
@@ -42,12 +38,37 @@ namespace Grades
             WriteResult("High Score", stats.HighGrade);
             WriteResult("Low Score", stats.LowGrade);
             WriteResult(stats.Description, stats.LetterGrade);
-
-            Console.ReadKey();
-
-
-            
         }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            StreamWriter outputFile = File.CreateText("Grades");
+            book.WriteGrades(outputFile);
+            outputFile.Close();
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(40);
+            book.AddGrade(80.1f);
+            book.AddGrade(76);
+            book.AddGrade(100);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
+            try
+            {
+                Console.WriteLine("Ïntrodu un nume");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
         static void WriteResult(string descriere, string rezultat)
         {
             Console.WriteLine($"{descriere}:{rezultat}", descriere, rezultat);
